9fa674a8fed1bdd003acda8a8279396cfaa1e63d
Marieve <45262901+marievegillian@users.noreply.github.com>
2024-05-30 07:21:48 +0800

    bug fix: no colon after display or scan

--- src/code/Parser.java
+++ src/code/Parser.java
@@ -172,6 +172,9 @@ public class Parser {
         
 
         if (match(DISPLAY)) {
+            if (!match(COLON)) {
+                throw error(peek(), "Expect ':' after 'DISPLAY'.");
+            }
             if(match(COLON)){
                 executableCodeStart = true;
                 return displayStatement();
@@ -179,6 +182,9 @@ public class Parser {
         }
 
         if (match(SCAN)) {
+            if (!match(COLON)) {
+                throw error(peek(), "Expect ':' after 'SCAN'.");
+            }
             if(match(COLON)){
                 executableCodeStart = true;
                 return scanStatement();
